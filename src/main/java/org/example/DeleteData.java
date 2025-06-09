5406decfd01a996905f27fd5720ba5d52f178422
TusharUmbardand <tusharumbardand1@gmail.com>
2025-06-09 18:32:13 +0530

    Delete data

---
+++ src/main/java/org/example/DeleteData.java
@@ -0,0 +1,32 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DeleteData {
+    public static void main(String[] args) {
+        String url = "jdbc:postgresql://localhost:5432/shop";
+        String user = "postgres";
+        String passWord = "1234";
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection conn = DriverManager.getConnection(url,user,passWord);
+
+            String query = "delete from product where id = 1";
+            Statement statement = conn.createStatement();
+
+            statement.execute(query);
+
+            conn.close();
+
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
